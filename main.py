d56bb81fb66e9eb84875f5dc053856e44c5ac828
Truc Trinh <ttrinh2@umbc.edu>
2021-10-30 13:53:02 -0400

    Explore widgets as DisplayaText & Dictionarydata

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
+
+## simple hello world!!!
 st.write ("Howdy World! My Name is Curt\n")
+
+import random
+import pandas as pd
+
+## explore_widgets 
+#Display a Text
+st.title ("Demo streamlit widgets\n")
+st.header ("Display Python variables\n")
+
+my_var = 10
+st.write(my_var)
+
+#Dictionary of data
+data ={"first_name": "Curt", "last_name":"Hnirt"}
+st.write(data)
